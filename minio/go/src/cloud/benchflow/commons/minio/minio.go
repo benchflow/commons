978af11b0d46b9f36fdaa71db006392abb2d0f27
Cerfoglg <gabrielecerfoglio225@gmail.com>
2016-06-16 19:21:39 +0200

    minio port in both functions

--- minio/go/src/cloud/benchflow/commons/minio/minio.go
+++ minio/go/src/cloud/benchflow/commons/minio/minio.go
@@ -30,8 +30,8 @@ func callMinioClient(fileName string, minioHost string, minioKey string) {
 }
 */
 
-func SendGzipToMinio(fileName string, minioHost string, minioKey string, accessKey string, secretAccessKey string) {
-	sendToMinio(fileName, minioHost, minioKey, accessKey, secretAccessKey, "application/gzip")
+func SendGzipToMinio(fileName string, minioHost string, minioPort string, minioKey string, accessKey string, secretAccessKey string) {
+	sendToMinio(fileName, minioHost, minioPort, minioKey, accessKey, secretAccessKey, "application/gzip")
 	}
 
 func sendToMinio(fileName string, minioHost string, minioPort string, minioKey string, accessKey string, secretAccessKey string, fileType string) error {
