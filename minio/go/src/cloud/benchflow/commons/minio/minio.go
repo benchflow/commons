941e2e810e4bd1eb6fac250793be2b0f043f65d9
Cerfoglg <gabrielecerfoglio225@gmail.com>
2016-06-16 19:20:34 +0200

    minio port as paramether

--- minio/go/src/cloud/benchflow/commons/minio/minio.go
+++ minio/go/src/cloud/benchflow/commons/minio/minio.go
@@ -34,8 +34,8 @@ func SendGzipToMinio(fileName string, minioHost string, minioKey string, accessK
 	sendToMinio(fileName, minioHost, minioKey, accessKey, secretAccessKey, "application/gzip")
 	}
 
-func sendToMinio(fileName string, minioHost string, minioKey string, accessKey string, secretAccessKey string, fileType string) error {
-	minioClient, err := minio.New(minioHost+":9000", accessKey, secretAccessKey, false)
+func sendToMinio(fileName string, minioHost string, minioPort string, minioKey string, accessKey string, secretAccessKey string, fileType string) error {
+	minioClient, err := minio.New(minioHost+":"+minioPort, accessKey, secretAccessKey, false)
 	if err != nil {
 		fmt.Println(err)
 	    return err
